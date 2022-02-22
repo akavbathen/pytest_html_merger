d7f5251ce0ea9ab95bc7cf678646d9789791287d
Akav Bat Hen <akav.bathen@gmail.com>
2022-02-22 18:50:30 +0200

    wip

--- gen_ver.py
+++ gen_ver.py
@@ -1,9 +1,9 @@
 import yaml
 
-with open("src/ver.yml", "r") as f:
+with open("pytest_html_merger/ver.yml", "r") as f:
     data = yaml.safe_load(f)
 
-with open("src/version.py", "w+") as f:
+with open("pytest_html_merger/version.py", "w+") as f:
     f.write('name = "{0}"\n'.format(data["name"]))
     f.write('version = "{0}"\n'.format(data["version"]))
     f.write('_version = "{0}"\n'.format(data["_version"]))
