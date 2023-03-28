12e2ecddfa0d8cf754415c0e9641be0e10955899
Next Alone <12210746+NextAlone@users.noreply.github.com>
2023-03-28 16:28:40 +0800

    feat: follow pytest-html 4.0.0rc changes

--- pytest_html_merger/main.py
+++ pytest_html_merger/main.py
@@ -1,19 +1,16 @@
-import re
 import argparse
+import json
 import os
-from bs4 import BeautifulSoup
-import sys
 import pathlib
+import sys
 
+from bs4 import BeautifulSoup
+
+import version as version_mod
 
 CUR_PATH = "{0}/".format(os.path.dirname(__file__))
 
 sys.path.append(CUR_PATH)
-
-import version as version_mod
-
-
-CHECKBOX_REGEX = r"^(?P<num>0|[1-9]\d*) (?P<txt1>.*)"
 
 
 def merge_html_files(in_path, out_path):
@@ -23,7 +20,7 @@ def merge_html_files(in_path, out_path):
 
     assets_dir_path = get_assets_path(in_path)
 
-    first_file = BeautifulSoup("".join(open(paths[0])), features="html.parser")
+    first_file = BeautifulSoup("".join(open(paths[0], encoding='utf-8')), features="html.parser")
 
     try:
         first_file.find("link").decompose()
@@ -44,96 +41,26 @@ def merge_html_files(in_path, out_path):
             head.append(first_file.new_tag("style", type="text/css"))
             head.style.append(content)
 
-    h = first_file.find("h1")
-    h.string = os.path.basename(out_path)
-
-    t = first_file.find("table", {"id": "results-table"})
-
-    cb_types = {
-        "passed": [0, ""],
-        "skipped": [0, ""],
-        "failed": [0, ""],
-        "error": [0, ""],
-        "xfailed": [0, ""],
-        "xpassed": [0, ""],
-    }
-
-    for cb_type in cb_types:
-        tmp = get_checkbox_value(first_file, cb_type)
-        cb_types[cb_type][0] = tmp[0]
-        cb_types[cb_type][1] = tmp[1]
-
-    test_count = 0
-    dur = 0
-    ps = first_file.find_all("p")
-    for p in ps:
-        if " tests ran" in p.text:
-            tmp = p.text.split(" ")
-            test_count = int(tmp[0])
-            dur = float(tmp[4])
-
-            break
+    dataset = first_file.find('div', {'id': 'data-container'})
+    dataset_json = json.loads(dataset.attrs['data-jsonblob'])
+    dataset_json['title'] = os.path.basename(out_path)
 
     for path in paths:
         if path == paths[0]:
             continue
 
-        second_file = BeautifulSoup("".join(open(path)), features="html.parser")
+        second_file = BeautifulSoup(
+            "".join(open(path, encoding='utf-8')), features="html.parser")
+        res = second_file.find('div', {'id': 'data-container'})
+        dataset_json2 = res.attrs['data-jsonblob']
+        dataset_json2 = json.loads(dataset_json2)
+        dataset_json['tests'] = {
+            **dataset_json['tests'], **dataset_json2['tests']}
+        dataset_json['collectedItems'] += dataset_json2['collectedItems']
 
-        res = second_file.find_all("tbody", {"class": "results-table-row"})
-        for elm in res:
-            t.append(elm)
-
-        res = second_file.find_all("p")
-        for p in res:
-            if " tests ran" in p.text:
-                tmp = p.text.split(" ")
-                test_count += int(tmp[0])
-                dur += float(tmp[4])
-
-                break
-
-        for cb_type in cb_types:
-            tmp = get_checkbox_value(second_file, cb_type)
-            cb_types[cb_type][0] += tmp[0]
-
-    res = first_file.find_all("p")
-    for p in res:
-        if " tests ran" in p.text:
-            p.string = f"{test_count} tests ran in {dur} seconds"
-
-            break
-
-    for cb_type in cb_types:
-        set_checkbox_value(first_file, cb_type, cb_types[cb_type])
-
-    with open(out_path, "w") as f:
+    first_file.find('div', {'id': 'data-container'}).attrs['data-jsonblob'] = json.dumps(dataset_json)
+    with open(out_path, "w", encoding='utf-8') as f:
         f.write(str(first_file))
-
-
-def set_checkbox_value(root_soap, cb_type, val):
-    elem = root_soap.find("span", {"class": cb_type})
-    match = re.search(CHECKBOX_REGEX, elem.text)
-    if match is None:
-        raise RuntimeError(f"{cb_type} <span> not found")
-
-    elem.string = f"{val[0]} {val[1]}"
-
-    elem = root_soap.find("input", {"data-test-result": cb_type})
-    if val[0] != 0:
-        del elem["disabled"]
-        del elem["hidden"]
-
-
-def get_checkbox_value(root_soap, cb_type):
-    elem = root_soap.find("span", {"class": cb_type})
-    match = re.search(CHECKBOX_REGEX, elem.text)
-    if match is None:
-        raise RuntimeError(f"{cb_type} <span> not found")
-
-    gdict = match.groupdict()
-
-    return int(gdict["num"]), gdict["txt1"]
 
 
 def get_html_files(path):
@@ -144,7 +71,7 @@ def get_html_files(path):
         if "merged.html" in res:
             continue
 
-        tmp = BeautifulSoup("".join(open(res)), features="html.parser")
+        tmp = BeautifulSoup("".join(open(res, encoding='utf-8')), features="html.parser")
         p = tmp.find("p")
         if p and "Report generated on " in p.text:
             onlyfiles.append(res)
