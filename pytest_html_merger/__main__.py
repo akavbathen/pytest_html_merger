db7399a0df573d4fbfa641f879f951f0a1d529ee
Andrew Siemer <35670954+andrewsiemer@users.noreply.github.com>
2023-10-07 09:06:05 -0700

    Add ability to run as script with `python -m pytest_html_merger`

---
+++ pytest_html_merger/__main__.py
@@ -0,0 +1,7 @@
+"""Allow running as a script through `python -m pytest_html_merger`."""
+from __future__ import absolute_import
+
+from pytest_html_merger.main import main
+
+if __name__ == "__main__":
+    main()
