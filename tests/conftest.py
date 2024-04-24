88ac88c5c7c532b79a665dd98fe9be0f145b1a59
Bat Hen Akav <88984920+akavbathen@users.noreply.github.com>
2024-04-24 21:06:47 +0300

    test

---
+++ tests/conftest.py
@@ -0,0 +1,7 @@
+import pathlib
+
+import pytest
+
+
+def create_pytest_report(report_path: pathlib.Path, success: int, failed: int):
+    return ""
--- tests/test_merger.py
+++ tests/test_merger.py
@@ -1,5 +1,13 @@
+import pathlib
+
 import pytest
+
+import pytest_html_merger.main as phm
+
 from bs4 import BeautifulSoup
+
+from tests.conftest import create_pytest_report
+
 
 def test_title():
     html_file_path: str = "./merged.html"
@@ -12,6 +20,7 @@ def test_title():
     title = soup.title.string
     assert title == "merged.html"
 
+
 def test_h1_title():
     html_file_path: str = "./merged.html"
 
@@ -23,4 +32,25 @@ def test_h1_title():
     title = soup.select_one("#title").string
     assert title == "merged.html"
 
+
+def test_succeeded_and_failed(tmp_path):
+    subfolder: pathlib.Path = tmp_path / "results"
+    subfolder.mkdir()
+    file_name: pathlib.Path = subfolder / "result.html"
+
+    create_pytest_report(tmp_path, success=2, failed=2)
+    create_pytest_report(tmp_path, success=1, failed=1)
+    phm.main(
+        [
+            "--input",
+            str(tmp_path),
+            "-o",
+            str(file_name),
+            "-t",
+            "html_report"
+
+        ]
+    )
+
+    assert file_name.exists()
 
