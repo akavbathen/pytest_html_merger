b324e0cacd0098f7877ad158c9b56b4dde45bd30
Bat Hen Akav <akav.bathen@gmail.com>
2024-05-11 16:24:26 +0300

    finish to implement infra

--- tests/test_merger.py
+++ tests/test_merger.py
@@ -21,16 +21,35 @@ def test_title():
     assert title == "merged.html"
 
 
-def test_h1_title():
-    html_file_path: str = "./merged.html"
+def test_h1_title(custom_tmp_path):
+    subfolder: pathlib.Path = custom_tmp_path / "results"
+    subfolder.mkdir(exist_ok=True, parents=True)
+    file_name: pathlib.Path = subfolder / "result.html"
 
-    with open(html_file_path, "r") as f:
+    input_path = custom_tmp_path / "input_reports"
+    input_path.mkdir(exist_ok=True)
+    venv_path = custom_tmp_path / "venv4"
+
+    create_pytest_report(venv_path, input_path, success=10, failed=2)
+    create_pytest_report(venv_path, input_path, success=1, failed=1)
+    phm.main(
+        [
+            "--input",
+            str(input_path),
+            "-o",
+            str(file_name),
+            "-t",
+            "merged"
+
+        ]
+    )
+
+    with open(file_name, "r") as f:
         html_content: str = f.read()
 
     soup = BeautifulSoup(html_content, "html.parser")
-
     title = soup.select_one("#title").string
-    assert title == "merged.html"
+    assert title == "merged"
 
 
 def test_succeeded_and_failed(custom_tmp_path):
