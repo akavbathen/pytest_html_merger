a489be1865c111114e585257434c43c4839659b4
Bat Hen Akav <akav.bathen@gmail.com>
2024-05-14 14:51:32 +0300

    find and reproduce bug with num of tests
    relevant to version pytest >= 4.0.0

--- tests/test_merger.py
+++ tests/test_merger.py
@@ -1,12 +1,12 @@
 import pathlib
-
 import pytest
-
 import pytest_html_merger.main as phm
-
 from bs4 import BeautifulSoup
-
 from tests.conftest import create_pytest_report
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 def test_title():
@@ -112,5 +112,32 @@ def test_the_number_of_failures_indicated_in_the_report(custom_tmp_path, element
 
 
 
+def test_checking_the_number_of_failed_records(custom_tmp_path):
+    subfolder: pathlib.Path = custom_tmp_path / "results"
+    subfolder.mkdir(exist_ok=True, parents=True)
+    file_name: pathlib.Path = subfolder / "result.html"
 
+    input_path = custom_tmp_path / "input_reports"
+    input_path.mkdir(exist_ok=True)
+    venv_path = custom_tmp_path / "venv4"
 
+    create_pytest_report(venv_path, input_path, success=2, failed=2)
+    create_pytest_report(venv_path, input_path, success=1, failed=1)
+    phm.main(
+        [
+            "--input",
+            str(input_path),
+            "-o",
+            str(file_name),
+            "-t",
+            "html_report"
+
+        ]
+    )
+
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    file_path = f'file://{file_name}'
+    driver.get(file_path)
+
+    elements = driver.find_elements(By.CSS_SELECTOR, ".results-table-row.failed")
+    assert len(elements) == 3
